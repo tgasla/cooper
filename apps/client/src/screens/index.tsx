7dc8cf4da6f803a18b9191b6dfca519c63de7a15
Mark Doyle <mark@mdoyle.sh>
2024-11-18 23:32:36 +0000

    make timeline scrollable

--- apps/client/src/screens/index.tsx
+++ apps/client/src/screens/index.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { useHostQuery, Vm } from "../queries/host";
 import { useSimulationQuery } from "../queries/simulation";
 import { useTimeQuery } from "../queries/time";
 import { cn } from "../lib";
+import { motion } from "motion/react";
+import { useDrag, usePinch } from "@use-gesture/react";
 
 function Hosts({ simulationId }: { simulationId: string }) {
   const { data: hosts } = useHostQuery(simulationId);
@@ -61,6 +63,21 @@ function Index() {
   const { data: times } = useTimeQuery(simulationId);
   const [currentTime, setCurrentTime] = useState<number>();
   const { data: hosts } = useHostQuery(simulationId);
+  usePinch(
+    (e) => {
+      console.log(e);
+    },
+    {
+      target: container,
+    },
+  );
+
+  useDrag(
+    (state) => {
+      console.log(state);
+    },
+    { target: container },
+  );
 
   useEffect(() => {
     if (!simulationId && simulations && simulations.length > 0) {
@@ -91,38 +108,48 @@ function Index() {
     endTime.simulation_time_seconds - startTime.simulation_time_seconds;
 
   return (
-    <div className="border flex flex-col justify-between" ref={container}>
+    <div
+      className="border flex flex-col overflow-x-scroll max-w-screen relative"
+      ref={container}
+    >
       <div>{startTime.simulation_time_seconds}</div>
       <div>{endTime.simulation_time_seconds}</div>
-      <div className="w-full h-12">
+      <div className="flex" style={{ width: secondSize * end }}>
         {new Array(end).fill(0).map((_, t) => (
-          <div
-            key={t}
-            className="absolute border"
-            style={{
-              left: t * secondSize,
-              width: secondSize,
-            }}
-          >
-            {t + 1}
-          </div>
+          <Fragment key={t}>
+            <motion.div
+              key={t}
+              initial={false}
+              animate={{
+                width: secondSize,
+              }}
+            >
+              <span>{t + 1}</span>
+            </motion.div>
+            <div
+              className="absolute w-[1px] h-full bg-black/10"
+              style={{ left: secondSize * t }}
+            ></div>
+          </Fragment>
         ))}
       </div>
 
-      <div className="flex flex-col w-full gap-3">
+      <div className="flex flex-col gap-3">
         {hosts.map((host) => (
-          <div
+          <motion.div
             key={host.id}
             className={cn(
               "flex flex-col bg-gradient-to-t from-yellow-400 to-yellow-500",
               "h-12 rounded-lg border-yellow-600 border-1",
             )}
-            style={{
-              width: width(10, secondSize),
+            initial={false}
+            animate={{
+              opacity: 1,
+              width: width(15, secondSize),
             }}
           >
             {host.id}
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
